aa202ba6d1720f16e9612e3f6f304ddc15661ca3
DanielMarcello <daniel.salinas.tellez@gmail.com>
2018-05-08 09:27:23 -0700

    variables declarations

--- hello_world/src/helloworld.kt
+++ hello_world/src/helloworld.kt
@@ -2,5 +2,14 @@
  multiline comentary
  */
 fun main(args: Array<String>){ //comentary
-    println("Hello World!")
+    var int_number = 10
+    println(int_number)
+    int_number = 666
+    println(int_number)
+
+    var string_dec2: String
+    string_dec2 = "Hello"
+    println(string_dec2)
+    string_dec2 = "World!"
+    println(string_dec2)
 }
