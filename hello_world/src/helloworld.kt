f6c1de111ec3a91e038fcb25fa78be7260e60157
DanielMarcello <daniel.salinas.tellez@gmail.com>
2018-05-08 09:44:37 -0700

    classes and functions

--- hello_world/src/helloworld.kt
+++ hello_world/src/helloworld.kt
@@ -2,7 +2,7 @@
  multiline comentary
  */
 fun main(args: Array<String>){ //comentary
-    var int_number = 10
+    /*var int_number = 10
     println(int_number)
     int_number = 666
     println(int_number)
@@ -11,5 +11,17 @@ fun main(args: Array<String>){ //comentary
     string_dec2 = "Hello"
     println(string_dec2)
     string_dec2 = "World!"
-    println(string_dec2)
+    println(string_dec2)*/
+
+    var personobj = Person()
+    personobj.name = "Daniel"
+    personobj.display()
+}
+
+class Person(){
+    var name = ""
+
+    fun display(){
+        println("Hello, my name is ${this.name}")
+    }
 }
